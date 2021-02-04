f62ecd866fb92f1e687c1ec61d7bc30b37d0d9b2
Adam <adam.bene98@gmail.com>
2021-02-04 20:35:50 +0100

    funkcni aplikace
    ukladani stromu, funkce bfs

--- bfs.py
+++ bfs.py
@@ -16,13 +16,64 @@ def read(input_name):
             exit()
 
 
+def breadth_first(graph):
+    queue = []
+    visited = []
+    queue.append(graph.first_vertex)
+    visited.append(graph.first_vertex)
+
+    result = [[graph.first_vertex]]
+    currentLevel = []
+
+    while len(queue)!=0:
+        vertex = queue.pop(0)
+        if vertex in currentLevel:
+            result.append(currentLevel)
+            currentLevel = []
+        
+        for neighbour in vertex.neighbours:
+            if neighbour not in visited:
+                queue.append(neighbour)
+                visited.append(neighbour)
+                currentLevel.append(neighbour)
+
+    return result
+
+
+def save(file_name, tree):
+    try:
+        with open(file_name, "w", encoding="UTF-8") as physicFile:
+            for level_index in range(len(tree)):
+                row_str = ""
+
+                for vertex in tree[level_index]:
+                    if row_str=="":
+                        row_str = str(vertex.label)
+                    else:
+                        row_str += " " + str(vertex.label)
+
+                if level_index!=len(tree)-1:
+                    row_str += '\n'
+                    
+                physicFile.write(row_str)
+
+
+    except PermissionError:
+        print(f"CHYBA: Nemuzu ulozit vysledny soubor, protoze nemam pristup k ukladani.")
+        exit()
+    except:
+        print("CHYBA: Vysledny soubor se mi nepodarilo ulozit.")
+        exit()
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=False, default=None)
 parser.add_argument('-o', '--output', required=False, default=None)
 args = parser.parse_args()
 if args.input != None and args.output != None:
     lines = read(args.input)
-    G = Graph(lines)
+    result_tree = breadth_first(Graph(lines))
+    save(args.output, result_tree)
 else:
     print("Nezadali jste povinne argumenty (-i pro vstupni soubor, -o pro vystupni soubor.")
     exit()
--- graph.py
+++ graph.py
@@ -4,11 +4,14 @@ class Graph:
 
     def __init__(self, lines):
         self.vertices = {}
+        self.first_vertex = None
 
         for line in lines:
             [label1, label2] = line.split(" ")
             if label1 not in self.vertices:
                 self.vertices[label1] = Vertex(label1)
+                if self.first_vertex == None:
+                    self.first_vertex = self.vertices[label1]
             if label2 not in self.vertices:
                 self.vertices[label2] = Vertex(label2)
             self.vertices[label1].add_neighbour(self.vertices[label2])
